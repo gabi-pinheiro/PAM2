9dc10b2ac2519c87fd61cc4ba89c4edcb1e18c90
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2022-08-31 08:31:07 -0300

    Segundo commit

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, TextInput } from 'react-native';
 
-export default function App() {
+const gore = () =>{
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <ScrollView>
+        <View>
+          <Text 
+            style={{
+              fontsize: 45,
+              alignSelf: "center",
+              color:"green",
+            }}
+            >
+              oiiii
+            </Text>
+          <Image source={{url:"https://i.pinimg.com/474x/8e/a3/b5/8ea3b5d795e1efa22d4609a3d0f7b339.jpg"}}> </Image> 
+        </View>
+    </ScrollView>
+  )};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//  export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Teste</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'white',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//});
